0592fb4bbe69005f77aac1ded6831fd8574ada34
Zach Bogart <31786233+zachbogart@users.noreply.github.com>
2017-09-29 15:46:24 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,29 +1,22 @@
 /*********
-*
-* D3 example #02:
-* Using the margin convention
-* Circles
-* Dynamic SVG insertion
-* Reading in external data
-* selectAll/append/etc
-*
+chart.js
+CDSS-hackathon-2017
+September 23, 2017
+
+Zach Bogart
+Aakanksha Joshi
+Youyang Liu
+
 *********/
 
 (function () {
 
-  /*
-   
-   WE WILL USE THIS CODE LATER, LEAVE IT COMMENTED OUT FOR NOW
-
-  */
-
-  // Always cut and paste this code to get a height and width
-  // with some nice padding around the edges
+  //margins
   var margin = { top: 40, left: 100, right: 90, bottom: 50},
       height = 500 - margin.top - margin.bottom,
       width = 800 - margin.left - margin.right;
   
-  // Grab the SVG from the page, set the height and width
+  //chart
   var svg = d3.select("#chart")
       .append("svg")
       .attr("height", height + margin.top + margin.bottom)
@@ -31,7 +24,7 @@
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
+  //Make scales
   //year
   var xPositionScale = d3.scaleLinear()
     .domain([2000,2016])
@@ -42,42 +35,39 @@
     .domain([0,500000000000])
     .range([height,0])
 
-  //don't have to give this a domain
+  //color
   var colorScale = d3.scaleOrdinal()
     .range(['pink', 'lightblue', 'lightgreen'])
 
-  //contracts
+  //area
   var areaScale = d3.scaleSqrt()
     .domain([1,1200000])
     .range([5,50])
 
-
-  // Read in some external data. When we're done, run the function 'ready'
+  //read in data
   d3.queue()
     .defer(d3.csv, "data/results.csv")
     .await(ready)
 
-
+  //add axes
+  //yAxis
   var yAxis = d3.axisLeft(yPositionScale);
     svg.append("g")
       .attr("class", "axis y-axis")
       .call(yAxis);
+  
+  //xAxis
+  var xAxis = d3.axisBottom(xPositionScale)
+  svg.append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-    var xAxis = d3.axisBottom(xPositionScale)
-    svg.append("g")
-      .attr("class", "axis x-axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-
-  // This is 'ready':
-  // it receives an error (if there is one)
-  // and datapoints, our newly-read-in data
-
+  //let's draw
   function ready(error, countries) {
     // console.log(countries)
 
-    // d3 code goes here
+    //defense spending
     svg.selectAll(".defense")
       .data(countries)
       .enter().append("circle")
@@ -96,6 +86,7 @@
       })
       .attr("opacity", 0.25)
 
+    //NASA
     svg.selectAll(".nasa")
       .data(countries)
       .enter().append("circle")
@@ -114,6 +105,7 @@
       })
       .attr("opacity", 0.25)
 
+    //EPA
     svg.selectAll(".env")
       .data(countries)
       .enter().append("circle")
@@ -131,10 +123,6 @@
         return colorScale(d.type)
       })
       .attr("opacity", 0.25)
-
-    
-
-    
 
   }
 
